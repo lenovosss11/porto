b3910863eae035e8a2a418e689224d19b4e569c4
lenovosss11 <nofryansyahhery@gmail.com>
2024-03-27 14:58:09 +0700

    number2

--- src/content/certifications.js
+++ src/content/certifications.js
@@ -1,6 +1,14 @@
 //certifications start
 const certifications = [
   {
+    name: "Sertifikat Pendidikan Khusus Profesi Advokat",
+    description:
+      "Sertifikasi yang dikeluarkan oleh Perhimpunan Advokat Indonesia (PERADI) yang wajib ditempuh oleh setiap calon Advokat sesuai amanat Undang-Undang No. 18 Tahun 2003 tentang Advokat",
+    link: "/certifications/pkpa.jpg",
+    img: "/certifications/pkpa.jpg",
+    preview: "https://peradi.or.id/index.php/profil/detail/8",
+  },
+  {
     name: "Sertifikat AK3U",
     description:
       "Sertifikasi yang dikeluarkan oleh Kementerian Ketenagakerjaan Indonesia yang menunjukkan bahwa suatu individu telah memiliki kemahiran di bidang Keselamatan Kesehatan Kerja Umum (Ahli K3 Umum)",
@@ -8,9 +16,17 @@ const certifications = [
     img: "/certifications/ak3u.jpg",
     preview: "https://ahlik3.co.id//",
   },
-
+  {
+    name: "Sertifikat JLPT N5",
+    description:
+      "Sertifikat yang dikeluarkan oleh Japan Foundation sebagai pengakuan bahwa peserta memiliki Kemampuan N5 (Beginner) dalam Bahasa Jepang",
+    link: "/certifications/jlptn5.jpg",
+    img: "/certifications/jlptn5.jpg",
+    preview: "https://www.jlpt.jp/e/faq/index.html",
+  },
 
 ];
+
 
 //certifications end
 export default certifications;
--- src/content/information.js
+++ src/content/information.js
@@ -1,10 +1,10 @@
 //personal information start
 
 const userData = {
-  firstName: "<Hery",
-  lastName: "Nofryansyah/>",
+  firstName: "XxX< ヘリ",
+  lastName: "ノブリアン />XxX",
   title: "About Me",
-  img: "/pasfot.jpeg",
+  img: "/headfoto.jpeg",
   description:
     "Hey there! Im Hery, A Versatile professional with over 10 years Experience in IT, 2 years in legal practice, and a keen interest in HR and K3 (Health and Safety) roles. Specializes in technical troubleshooting, corporate law, and workplace safety, backed by Ahli K3 Umum and PKPA certifications. Proven ability in innovative solutions, legal support, and promoting workplace safety standards. Aiming for roles in Legal, IT, HR, or K3 staff to leverage comprehensive expertise and contribute to organizational success.",
 };
